7141bccdc354ec541edd0b8581ac3eaae0d5fdb7
Asif Ul Islam <aseasifislam@gmail.com>
2021-09-28 11:06:51 +0600

    Feat: phone verification

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Backend\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('user', [UserController::class, 'create']);
+Route::post('user', [UserController::class, 'store'])->name('user.store');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
